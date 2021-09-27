af077df8ce0a05f6a8e1ac12d85faf48fffccf05
quyx13 <ostendorf@gmx.com>
2021-09-27 10:43:22 +0200

    z

--- UI-PrototypeMoviesDBv0.6WPF/MainWindow.xaml.cs
+++ UI-PrototypeMoviesDBv0.6WPF/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace UI_PrototypeMoviesDBv0._6WPF
 {
@@ -7,6 +9,59 @@ namespace UI_PrototypeMoviesDBv0._6WPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(/*16*/100);
+            dispatcherTimer.Tick += timer_Tick;
+            dispatcherTimer.Start();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void menuExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void menuInfo_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnSettings_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnTest_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            
         }
     }
 }
