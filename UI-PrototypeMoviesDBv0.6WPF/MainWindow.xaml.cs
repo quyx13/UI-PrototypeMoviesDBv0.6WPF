8f9c4218b5250dc48c3ff38868c9a7b72a4476a9
quyx13 <ostendorf@gmx.com>
2021-09-27 10:35:43 +0200

    Projektdateien hinzufügen.

---
+++ UI-PrototypeMoviesDBv0.6WPF/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace UI_PrototypeMoviesDBv0._6WPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
