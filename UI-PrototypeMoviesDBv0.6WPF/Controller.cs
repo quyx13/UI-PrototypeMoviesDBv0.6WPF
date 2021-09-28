d3aad3cae33a977ac08f03668f0389fdf46f4c33
quyx13 <ostendorf@gmx.com>
2021-09-28 07:57:09 +0200

    z

--- UI-PrototypeMoviesDBv0.6WPF/Controller.cs
+++ UI-PrototypeMoviesDBv0.6WPF/Controller.cs
@@ -37,7 +37,7 @@ namespace UI_PrototypeMoviesDBv0._6WPF
 
         public void OnCounterChanged(object sender, EventArgs e)
         {
-
+            System.Diagnostics.Trace.WriteLine($"{_worker.GetCounter()}");
         }
 
         public void OnPrimeFound(object sender, EventArgs e)
