b5df440671dd97980f1a7695003312b13caef381
quyx13 <ostendorf@gmx.com>
2021-09-28 12:06:29 +0200

    z

---
+++ UI-PrototypeMoviesDBv0.6WPF/Model/Log.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace UI_PrototypeMoviesDBv0._6WPF.Model
+{
+    class Log
+    {
+        private Dictionary<string, object> _log = new Dictionary<string, object>();
+
+        public Dictionary<string, object> GetLog()
+        {
+            return _log;
+        }
+
+        public void AddLog(string s, object o)
+        {
+            _log.Add(s, o);
+        }
+    }
+}
