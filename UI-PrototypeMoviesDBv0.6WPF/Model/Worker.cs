0d08dd6b29d11641a9f8f007641e698dc11b9fde
quyx13 <ostendorf@gmx.com>
2021-09-28 13:52:37 +0200

    z

--- UI-PrototypeMoviesDBv0.6WPF/Model/Worker.cs
+++ UI-PrototypeMoviesDBv0.6WPF/Model/Worker.cs
@@ -13,6 +13,13 @@ namespace UI_PrototypeMoviesDBv0._6WPF.Model
         public event EventHandler CounterChanged;
         public event EventHandler WorkerDone;
 
+        public event EventHandler All;
+        public event EventHandler Even;
+        public event EventHandler Uneven;
+        public event EventHandler Modulo13;
+        public event EventHandler Modulo100;
+        public event EventHandler Prime;
+
         #region Getter and setter
         public int GetCounter()
         {
@@ -53,6 +60,21 @@ namespace UI_PrototypeMoviesDBv0._6WPF.Model
                     Thread.Sleep(_wait);
 
                     CounterChanged?.Invoke(this, EventArgs.Empty);
+                    All?.Invoke(this, EventArgs.Empty);
+
+                    if (_counter % 2 == 0)
+                        Even?.Invoke(this, EventArgs.Empty);
+                    else
+                        Uneven?.Invoke(this, EventArgs.Empty);
+
+                    if (_counter % 13 == 0)
+                        Modulo13?.Invoke(this, EventArgs.Empty);
+
+                    if (_counter % 100 == 0)
+                        Modulo100?.Invoke(this, EventArgs.Empty);
+
+                    if (IsPrime(_counter))
+                        Prime?.Invoke(this, EventArgs.Empty);
                     #endregion
                 }
                 if (_workerState == WorkerState.abort)
@@ -63,6 +85,21 @@ namespace UI_PrototypeMoviesDBv0._6WPF.Model
             }
 
             WorkerDone?.Invoke(this, EventArgs.Empty);
+        }
+
+        private static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return false;
+
+            return true;
         }
     }
 }
