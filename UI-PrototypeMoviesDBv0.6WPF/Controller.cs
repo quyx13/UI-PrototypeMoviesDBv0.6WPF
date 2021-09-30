af08dd502967f7ef238a46e4f6bcee9f5a58d093
quyx13 <ostendorf@gmx.com>
2021-09-30 17:14:06 +0200

    z

--- UI-PrototypeMoviesDBv0.6WPF/Controller.cs
+++ UI-PrototypeMoviesDBv0.6WPF/Controller.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using UI_PrototypeMoviesDBv0._6WPF.Model;
