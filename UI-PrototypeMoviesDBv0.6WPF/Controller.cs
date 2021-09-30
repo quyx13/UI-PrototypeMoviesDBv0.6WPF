d8e1a4540128324ba2aed55ee93821cab88f0ffd
quyx13 <ostendorf@gmx.com>
2021-09-30 12:07:39 +0200

    z

--- UI-PrototypeMoviesDBv0.6WPF/Controller.cs
+++ UI-PrototypeMoviesDBv0.6WPF/Controller.cs
@@ -98,6 +98,7 @@ namespace UI_PrototypeMoviesDBv0._6WPF
                     _mainWindow.SetState(WorkerState.ready);
                     _mainWindow.SetupStatusProgressBar(0, 1, 0);
                     _mainWindow.ClearComboBoxItems();
+                    _mainWindow.ClearTextBox();
                     _updates.Clear();
                     ClearLog();
                     Log("reset");
@@ -150,8 +151,6 @@ namespace UI_PrototypeMoviesDBv0._6WPF
         #region Log
         private void ClearLog()
         {
-            SaveLogToFile();
-
             _log = new Dictionary<string, List<string>>() { { "Output", new List<string>() } };
             _logText = new Dictionary<string, string>() { { "Output", string.Empty } };
         }
