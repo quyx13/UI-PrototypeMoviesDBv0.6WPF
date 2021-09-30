a1246b6311c680fe0f537547fe2fea197becc362
quyx13 <ostendorf@gmx.com>
2021-09-30 10:10:22 +0200

    z

---
+++ UI-PrototypeMoviesDBv0.6WPF/Model/Support.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UI_PrototypeMoviesDBv0._6WPF.Model
+{
+    public enum WorkerState
+    {
+        ready,
+        running,
+        stopped,
+        done,
+        abort
+    }
+
+    public class ListEventArgs : EventArgs
+    {
+        public List<string> Data { get; set; }
+        public ListEventArgs(List<string> data)
+        {
+            Data = data;
+        }
+    }
+}
